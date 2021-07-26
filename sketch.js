380bde68f61d318148e95550d0f4b6c0582a32c0
Amber Hurwitz <53446399+Hurwa134@users.noreply.github.com>
2021-07-25 20:50:46 -0400

    Waterfall Code 4

--- sketch.js
+++ sketch.js
@@ -1,52 +1,63 @@
-let squareSize = 3;
+let lineWidth = 4;
+let lineLength = lineWidth * 3;
 let pattern = [];
 let lastrowIndex = 0;
 let screenWidth = 0;
 let screenHeight = 0;
 let waterfallWidth = 0;
+//position of second changed row, in fraction of the waterfall height
+let changeProportion= 0.5;
+let secondchangedRow;
+let lineRandomized = 0.05;
+
+//framerate
+let fr = 7;
 
 rowNum = 0;
 squareNum = 0; 
 
 
 function setup() {
-  // background(0);
-  
+//(frames per second)
+frameRate(fr);
    screenWidth = windowWidth;
    screenHeight = windowHeight;
    waterfallWidth = screenWidth/3;
   createCanvas(screenWidth, screenHeight);
-  rowNum = ceil(screenHeight/squareSize) ;
-  squareNum = ceil(waterfallWidth/squareSize);
+  rowNum = ceil(screenHeight/lineLength) ;
+  squareNum = ceil(waterfallWidth/lineWidth);
+  //setting the next randomized row at a proportional point in the waterfallHeight
+  secondchangedRow = int(rowNum * changeProportion);
 }
-
-
 
 function draw() { 
 background(0);
 stroke(255);
 fill(255);
 rect(screenWidth/2 -waterfallWidth/2, 0, waterfallWidth, screenHeight);
-row0(screenWidth/2 -waterfallWidth/2,0); 
-
+//setting first row0 at top
+if (lastrowIndex == 0) {
+  randomRow(0);
+}
   pattern[lastrowIndex +1] = [];
     for (let i = 0; i <= lastrowIndex; i++){
     shiftRow(lastrowIndex-i);
         }
-changeRow0();
+changeRow(0);
      if (lastrowIndex < rowNum){ 
     lastrowIndex = lastrowIndex + 1;
   }
-   
+  if (lastrowIndex >= secondchangedRow){
+    oppositeRow(secondchangedRow);
+  }
 }
 
-function row0(topLeftX,topLeftY){
-  // randomSeed(99);
+//randomly sets row 0 the first time
+function randomRow(rowIndex){
+  squarestartX = screenWidth/2 -waterfallWidth/2;
+  squarestartY = rowIndex * lineLength;
   
-  squarestartX = topLeftX;
-  squarestartY = topLeftY;
-  
-  pattern[0]=[];
+  pattern[rowIndex]=[];
   for (let i = 0; i < squareNum; i++) {
   
   myColor = random(0,255);
@@ -55,32 +66,46 @@ function row0(topLeftX,topLeftY){
 } else {
   myColor = 255;
 }
-  
-    pattern[0][i] = int(myColor);
-  stroke(myColor);
-  fill(myColor);
-  square(squarestartX, squarestartY,squareSize);
-   
-    squarestartX = squarestartX + squareSize; 
+  //matrix that holds the colors
+    pattern[rowIndex][i] = myColor;
+    randomLine(squarestartX, squarestartY, squarestartX, squarestartY + lineLength, myColor, lineWidth);
+    squarestartX = squarestartX + lineWidth; 
     
     } 
 }
 
+function oppositeRow(rowIndex){
+  squarestartX = screenWidth/2 -waterfallWidth/2;
+  squarestartY = rowIndex * lineLength;
+  
+  for (let i = 0; i < squareNum; i++) {
+  myColor = pattern[rowIndex][i];
+  if (myColor == 255) {
+  myColor = 0;
+  }
+ else {
+  myColor = 255;
+}
+  //matrix that holds the colors
+    pattern[rowIndex][i] = myColor;
+    stroke(myColor);
+    strokeWeight(lineWidth);
+    randomLine(squarestartX, squarestartY, squarestartX, squarestartY + lineLength, myColor, lineWidth);
+   
+    squarestartX = squarestartX + lineWidth; 
+    } 
+}
+
+//shifts row down one
 function shiftRow(rowIndex){
   squarestartX = screenWidth/2 -waterfallWidth/2;
-  squarestartY = rowIndex * squareSize;
+  squarestartY = rowIndex * lineLength;
  
   for (let i = 0; i < squareNum; i++) {
   pattern[rowIndex+1][i]=pattern[rowIndex][i];
   myColor = pattern[rowIndex +1][i];
-  
-// pattern[rowIndex][i] = 255; 
-//   fill(255);
-//   square(squarestartX, squarestartY,squareSize);
-  stroke(myColor);
-  strokeWeight(squareSize);
-    line(squarestartX, squarestartY, squarestartX, squarestartY + squareSize);
-    squarestartX = squarestartX + squareSize; 
+  randomLine(squarestartX, squarestartY, squarestartX, squarestartY + lineLength, myColor, lineWidth);
+  squarestartX = squarestartX + lineWidth; 
     } 
 }
 
@@ -126,6 +151,8 @@ function squareColor(squareRow,squareColumn){
   if(oldColor[0] == oldColor[1]){
     return oldColor[2];
    }
+   //introducing grey scale
+  //  return random(0,255);
 
   a = random(0,255);
   if (a < 127) {
@@ -136,12 +163,21 @@ function squareColor(squareRow,squareColumn){
    }
   }
 
-function changeRow0(){
+function changeRow(rowNumber){
   for (let i = 0; i < squareNum; i++){
-   newColor = squareColor(0,i);
-    stroke(newColor);
-    strokeWeight(squareSize);
-    line(squarestartX, squarestartY, squarestartX, squarestartY + squareSize);
+   newColor = squareColor(rowNumber,i);
+    randomLine(squarestartX, squarestartY, squarestartX, squarestartY + lineLength, newColor, lineWidth);
     pattern[0][i] = newColor;
   }   
+}
+
+function randomLine(x1, y1, x2,y2, color, width){
+  stroke(color);
+  strokeWeight(width);
+  shift = lineRandomized * lineLength;
+  x1shift = random(-shift,shift);
+  y1shift = random(-shift,shift);
+  x2shift = random(-shift,shift);
+  y2shift = random(-shift,shift);
+  line (x1 + x1shift, y1 + y1shift, x2 + x2shift, y2 + y2shift);
 }
